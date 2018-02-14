6d212555780f3b25f459c27c13a469ce1cefc229
Muhand Jumah <muhandjumah@gmail.com>
2018-02-14 11:42:57 -0500

    Finished index.js

---
+++ src/index.js
@@ -0,0 +1,56 @@
+//Requires
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+//Global variables/objects
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+//Let variables
+let window;
+
+//////////////////////////
+//Events handeling
+//////////////////////////
+app.on('ready',
+  createWindow
+);
+
+app.on('window-all-closed',
+  () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  }
+);
+
+app.on('activate',
+  () => {
+    if (win === null) {
+      createWindow()
+    }
+  }
+);
+
+//////////////////////////
+//Utility methods
+//////////////////////////
+function createWindow(arguments) {
+  window = new BrowserWindow();
+
+  //Load window
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  //Garbage collection  if window is closed
+  window.on('closed',
+    (arguments) => {
+      window = null;
+    }
+  );
+
+}
